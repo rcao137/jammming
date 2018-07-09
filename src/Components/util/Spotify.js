5fa81944ad5345c5c3bedc3e55b537effb2afad4
Rong  Cao <rongcao@Rongs-MacBook-Pro-10.local>
2018-07-09 00:07:07 -0700

    fix accesstoken and expiresIn issues

--- src/Components/util/Spotify.js
+++ src/Components/util/Spotify.js
@@ -10,8 +10,8 @@ const Spotify = {
       const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
       const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
       if (accessTokenMatch && expiresInMatch) {
-        accessToken = accessTokenMatch;
-        const expiresIn = expiresInMatch;
+        accessToken = accessTokenMatch[1];
+        const expiresIn = parseInt(expiresInMatch[1], 10);
         window.setTimeout(() => accessToken = '', expiresIn * 1000);
         window.history.pushState('Access Token', null, '/');
         return accessToken;
